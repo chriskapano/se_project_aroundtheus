bc59483536bfe2b1b76d07ed6db8b8bdefefa6f6
Chris Kapano <kapanochris@yahoo.com>
2023-09-08 14:48:21 -0700

    .modal_opened corrected

--- scripts/index.js
+++ scripts/index.js
@@ -44,7 +44,7 @@ const cardTemplate =
 // FUNCTIONS
 
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -70,7 +70,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileEditCloseButton.addEventListener("click", closePopup);
